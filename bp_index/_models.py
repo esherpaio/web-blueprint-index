7ec103d4a92b68836869e878987ecc3458fbef5e
Stan Mertens <stnmertens@gmail.com>
2026-01-06 15:22:57 +0700

    lastmod on index

--- bp_index/_models.py
+++ bp_index/_models.py
@@ -4,12 +4,17 @@ from web.app.urls import url_for
 
 
 class Sitemap:
-    def __init__(self, endpoint: str) -> None:
+    def __init__(self, endpoint: str, updated_at: datetime) -> None:
         self._endpoint = endpoint
+        self._updated_at = updated_at
 
     @property
     def loc(self) -> str:
         return url_for(self._endpoint, _external=True)
+
+    @property
+    def lastmod(self) -> str:
+        return self._updated_at.strftime("%Y-%m-%d")
 
 
 class SitemapUrl:
