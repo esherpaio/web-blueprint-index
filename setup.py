ba3b2f1cd93295e8f4e5fc5d4bf973bcd4b34041
Stan Mertens <stnmertens@gmail.com>
2026-03-26 11:17:43 +0100

    fix sitemaps

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ from version import __version__
 
 DATA = [
     "*.md",
-    "static/**/*.css",
-    "static/**/*.js",
-    "templates/**/*.html",
+    "templates/**/*.txt",
+    "templates/**/*.xml",
 ]
 
 
