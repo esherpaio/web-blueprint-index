68e112fbb1670f0f2604bf8e325e6e44de6b5155
Stan Mertens <stnmertens@gmail.com>
2026-08-14 13:46:25 +0200

    Sitemap update

--- bp_index/models.py
+++ bp_index/models.py
@@ -4,24 +4,15 @@ from web.app.urls import url_for
 
 
 class Sitemap:
-    def __init__(self, endpoint: str, updated_at: datetime) -> None:
-        self._endpoint = endpoint
-        self._updated_at = updated_at
-
-    @property
-    def loc(self) -> str:
-        return url_for(self._endpoint, _external=True)
-
-    @property
-    def lastmod(self) -> str:
-        return self._updated_at.strftime("%Y-%m-%d")
-
-
-class SitemapUrl:
-    def __init__(self, endpoint: str, updated_at: datetime, **endpoint_args) -> None:
+    def __init__(
+        self,
+        endpoint: str,
+        lastmod: datetime,
+        **endpoint_args: str,
+    ) -> None:
         self._endpoint = endpoint
         self._endpoint_args = endpoint_args
-        self._updated_at = updated_at
+        self._lastmod = lastmod
 
     @property
     def loc(self) -> str:
@@ -29,4 +20,4 @@ class SitemapUrl:
 
     @property
     def lastmod(self) -> str:
-        return self._updated_at.strftime("%Y-%m-%d")
+        return self._lastmod.strftime("%Y-%m-%d")
--- bp_index/routes/sitemap.py
+++ bp_index/routes/sitemap.py
@@ -1,29 +1,35 @@
 import itertools
-from datetime import datetime, timezone
-from typing import Optional
+from datetime import datetime
 
 from flask import Response, abort, make_response, render_template
+from sqlalchemy import true
+from sqlalchemy.orm import joinedload
 from web.app.routing import has_argument, is_endpoint
+from web.app.urls import url_for
 from web.cache import cache
+from web.database import conn
+from web.database.model import AppRoute, SitemapLocation
 from web.locale import gen_locale
 from web.utils.modifiers import text_to_xml
 
 from bp_index import index_bp
-from bp_index.models import Sitemap, SitemapUrl
-
-
-def get_latest_date(*dates: Optional[datetime]) -> datetime:
-    return max(filter(None, dates), default=datetime.now(timezone.utc))
+from bp_index.models import Sitemap
 
 
 @index_bp.route("/sitemap.xml")
 def sitemap() -> Response:
-    sitemaps = []
-    for endpoint in ["index.sitemap_pages"]:
-        urls = _get_sitemap_urls()
-        if urls:
-            updated_at = get_latest_date(*(url._updated_at for url in urls))
-            sitemaps.append(Sitemap(endpoint, updated_at))
+    group_lastmods: dict[str, datetime] = {}
+    for location in _get_sitemap_location_rows():
+        group = location.route.sitemap_group
+        group_lastmods[group] = max(
+            group_lastmods.get(group, location.lastmod),
+            location.lastmod,
+        )
+
+    sitemaps = [
+        Sitemap("index.sitemap_group", lastmod, group=group)
+        for group, lastmod in sorted(group_lastmods.items())
+    ]
     if not sitemaps:
         abort(404)
 
@@ -33,29 +39,46 @@ def sitemap() -> Response:
     return response
 
 
-@index_bp.route("/sitemap-pages.xml")
-def sitemap_pages() -> Response:
-    urls = _get_sitemap_urls()
-    if not urls:
+@index_bp.route("/sitemap-<string:group>.xml")
+def sitemap_group(group: str) -> Response:
+    locations = _get_sitemap_locations(group)
+    if not locations:
         abort(404)
-    return _generate_sitemap(urls)
+    return _generate_sitemap(locations)
 
 
-def _get_sitemap_urls() -> list[SitemapUrl]:
+def _get_sitemap_location_rows(group: str | None = None) -> list[SitemapLocation]:
+    with conn.begin() as s:
+        query = (
+            s.query(SitemapLocation)
+            .join(SitemapLocation.route)
+            .options(joinedload(SitemapLocation.route))
+            .filter(AppRoute.in_sitemap == true())
+        )
+        if group is not None:
+            query = query.filter(AppRoute.sitemap_group == group)
+        sitemap_locations = query.order_by(
+            AppRoute.sitemap_group,
+            SitemapLocation.route_id,
+            SitemapLocation.id,
+        ).all()
+
+    return [
+        location
+        for location in sitemap_locations
+        if is_endpoint(location.route.endpoint)
+    ]
+
+
+def _get_sitemap_locations(group: str) -> list[tuple[SitemapLocation, str]]:
     locale_iter_args = (
         [x for x in cache.countries if x.in_sitemap],
         [x for x in cache.languages if x.in_sitemap],
     )
 
-    urls = []
-    for route in cache.routes:
-        if not route.in_sitemap or not is_endpoint(route.endpoint):
-            continue
-        if route.is_collection and (
-            not route.sitemap_query_key or not route.sitemap_query_values
-        ):
-            continue
-
+    locations = []
+    for sitemap_location in _get_sitemap_location_rows(group):
+        route = sitemap_location.route
         # Collect locale arguments
         if has_argument(route.endpoint, "_locale"):
             locale_args = [
@@ -65,24 +88,19 @@ def _get_sitemap_urls() -> list[SitemapUrl]:
         else:
             locale_args = [{}]
 
-        # Collect query arguments
-        if route.is_collection:
-            query_args = [
-                {route.sitemap_query_key: v} for v in route.sitemap_query_values
-            ]
-        else:
-            query_args = [{}]
+        # Build sitemap locations
+        for locale_args_ in locale_args:
+            endpoint_args = sitemap_location.endpoint_args | locale_args_
+            loc = url_for(route.endpoint, **endpoint_args, _external=True)
+            locations.append((sitemap_location, loc))
 
-        # Build sitemap URLs
-        updated_at = get_latest_date(route.updated_at, route.created_at)
-        for la, qa in itertools.product(locale_args, query_args):
-            urls.append(SitemapUrl(route.endpoint, updated_at=updated_at, **la, **qa))
-
-    return urls
+    return locations
 
 
-def _generate_sitemap(urls: list[SitemapUrl]) -> Response:
-    template = render_template("sitemap.xml", urls=urls)
+def _generate_sitemap(
+    locations: list[tuple[SitemapLocation, str]],
+) -> Response:
+    template = render_template("sitemap.xml", locations=locations)
     response = make_response(text_to_xml(template))
     response.headers["Content-Type"] = "application/xml"
     return response
